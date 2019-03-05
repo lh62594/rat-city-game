29eca5c4883182acac6b63461ddef6c720e9c641
github username <github email address>
2019-03-05 12:40:57 -0500

    adding background

--- test3.js
+++ test3.js
@@ -4,25 +4,22 @@
 const canvas = document.querySelector("#canvas");
 const fullWidth = 1000
 const fullHeight = 450
+const bgSpeed = -1
+const pizzaSpeed = -2.5
+const obSpeed = -4
+
 canvas.width = fullWidth
 canvas.height = fullHeight
 
-// let speed = -1
-// let bgSpeed = 0
 let c = canvas.getContext("2d");
 
+//arrays
 let obstacles = []
 let pizzas = []
 let bgs = []
 
-let controls = {
-  // left: false,
-  // up: false,
-  right: false,
-  // down: false,
-};
-
 let lives = 3
+let jumpKey = false
 
 
 /**************************************************
@@ -33,6 +30,7 @@ function createFloor() {
   c.fillStyle = "rgba(0,0,0)"
   c.fillRect(0, 250, fullWidth, 3);
 }
+
 createFloor()
 
 
@@ -43,7 +41,7 @@ class Obstacle {
   constructor(x, height) {
     this.x = x
     this.y = 250 - height
-    this.dx = -1
+    this.dx = obSpeed
     this.width = 50
     this.height = height
   }
@@ -53,22 +51,11 @@ class Obstacle {
     c.fillRect(this.x, this.y, this.width, this.height);
   }
 
-  stopped() {
-    if (this.x + 2*this.radius < 0) {
-      this.x = fullWidth + (Math.random()*400 + 375)
-      // this.delete()
-    }
-    this.x += this.dx
-    this.draw();
-    console.log(this.x);
-    console.log(this.dx);
-  }
-
-  moveRight() {
+  move() {
     if (this.x + this.width < 0) {
       this.x = fullWidth + (Math.random()*525 + 475)
     }
-    this.x += -5
+    this.x += this.dx
     this.draw();
   }
 
@@ -78,21 +65,14 @@ class Pizza {
   constructor(x) {
     this.x = x
     this.y = 225
-    this.dx = 0
+    this.dx = pizzaSpeed
     this.radius = 10
-    this.image = new Image();
-    this.image.src = "img/pizza.png"
+    // this.image = new Image();
+    // this.image.src = "img/pizza.png"
+    this.jumping = false
   }
 
   draw() {
-    // this.image.onload = function () {
-      // c.drawImage(this.image, 100, 100, 50, 50)
-    // }
-    // let img = new Image()
-    // img.src = 'img/pizza.png'
-    // img.onload = () => {
-    //   c.drawImage(img, this.x, this.y, 35, 35)
-    // }
     c.beginPath()
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
     c.fillStyle = "rgba(0, 0, 255, 0.7)"
@@ -101,19 +81,8 @@ class Pizza {
     c.fill()
   }
 
-  stopped() {
-    if (this.x + 2*this.radius < 0) {
-      this.x = fullWidth + (Math.random()*400 + 375)
-    }
-    // this.x += this.dx;
-    this.draw();
-  }
-
-  moveRight() {
-    if (this.x + 2*this.radius < 0) {
-      this.x = fullWidth + (Math.random()*400 + 375)
-    }
-    this.x += -2
+  move() {
+    this.x += this.dx
     this.draw();
   }
 
@@ -123,7 +92,7 @@ class Background {
   constructor(x) {
     this.x = x
     this.y = 75
-    this.dx = 0
+    this.dx = bgSpeed
     this.width = 60
     this.height = 25
     this.d = 500
@@ -140,34 +109,13 @@ class Background {
     c.strokeRect(this.d + this.x + this.width/2, this.y + 50, this.width, this.height)
     c.strokeRect(this.d + this.x, this.y + this.height + 50, this.width, this.height)
     c.strokeRect(this.d + this.x + this.width, this.y + this.height + 50, this.width, this.height)
-
-    // c.strokeRect(this.d*2 + this.x + this.width/2, this.y, this.width, this.height)
-    // c.strokeRect(this.d*2 + this.x, this.y + this.height, this.width, this.height)
-    // c.strokeRect(this.d*2 + this.x + this.width, this.y + this.height, this.width, this.height)
-    // let ctx = canvas.getContext("2d")
-    //
-    // // debugger
-    // ctx.drawImage(this.image,
-    //     this.x + this.width,
-    //     this.y,
-    //     this.width, this.height);
-  }
-
-  stopped() {
-    if (this.x + 2*this.radius < 0) {
-      this.x = fullWidth + (Math.random()*400 + 375)
-    }
-    // this.x += this.dx;
-    this.draw();
   }
 
   move() {
     if (this.x + this.d + 2 * this.width< 0) {
       this.x = fullWidth
     }
-    this.dx = -0.5
     this.x += this.dx;
-    // console.log(this.dx);
     this.draw();
   }
 } // end of Background class
@@ -182,6 +130,8 @@ class Player {
     this.height = 100
     this.gravity = 0
     this.gravitySpeed = 0
+    this.y_velocity = 0
+    this.jumping = false
   }
 
   draw() {
@@ -191,6 +141,19 @@ class Player {
 }
 
 
+
+// function jump() {
+//   if
+//   this.y_velocity -= 20
+//   this.jumping = true
+//   this.y_velocity += 1.5;
+//
+//   if (this.y > 250) {
+//     this.jumping = false
+//     this.y = 250
+//     this.y_velocity = 0
+//   }
+// }
 // function draw() {
 //   var ctx = document.getElementById('canvas').getContext('2d');
 //   var img = new Image();
@@ -203,6 +166,8 @@ class Player {
 /**************************************************
             BACKGROUND CREATIONS
 **************************************************/
+let player = new Player
+
 function createObstacles() {
   for (var i = 0; i < 2; i++) {
     var x = fullWidth + i * (Math.random()*525 + 475)
@@ -212,7 +177,7 @@ function createObstacles() {
 }
 
 function createPizzas() {
-  for (var i = 0; i < 3; i++) {
+  for (var i = 0; i < 10; i++) {
     var x = fullWidth + i * (Math.random()*400 + 375) + 350
     pizzas.push(new Pizza(x))
   }
@@ -225,9 +190,8 @@ function createBackground() {
   }
 }
 
-let player = new Player
 
-function firstRender() {
+function renderAll() {
   createObstacles()
   obstacles.forEach( o => o.draw() )
   createBackground()
@@ -244,72 +208,100 @@ function firstRender() {
 // to "move right" function
 // add event listeners on the left/right arrows
 // gives illusion that character is moving, without actually moving the character
-function moveRight() {
-  // requestAnimationFrame( () => {
-  //   moveRight()
-  // });
-  c.clearRect(0, 0, innerWidth, innerHeight);
-  createFloor();
-  player.draw()
-
-  obstacles.forEach( o => o.moveRight() )
-
-  pizzas.forEach( p => p.moveRight() )
-
-  bgs.forEach( b => b.move() )
-
-}
-
-function stop() {
+function animate() {
   requestAnimationFrame( () => {
-    stop()
+    animate()
   });
   c.clearRect(0, 0, innerWidth, innerHeight);
   createFloor();
   player.draw()
 
-  obstacles.forEach( o => o.stopped() )
+  obstacles.forEach( o => o.move() )
 
-  pizzas.forEach( p => {
-    p.draw()
-  })
-  //
-  bgs.forEach( b => {
-    b.draw()
-  })
+  pizzas.forEach( p => p.move() )
+
+  bgs.forEach( b => b.move() )
+
+
 }
 
+function playerJump() {
+  // for (var i = 0; i < 5; i++) {
+    if(jumpKey == true) {
+      player.y = 40
+    }
+    // console.log(player.y);
+    player.y += 10
+
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    createFloor();
+    player.draw()
+    animate()
+
+    if (player.y > 140) {
+      // player.jumping == false
+      player.y = 140
+      player.y_velocity = 0;
+
+    }
+  // }
+  requestAnimationFrame(playerJump)
+}
+
+
+function loop() {
+
+  if (player.jumping == false) {
+
+    player.y_velocity -= 20;
+    player.jumping = true;
+
+  }
+
+  player.y_velocity += 1.5;// gravity
+  player.x += player.x_velocity;
+  player.y += player.y_velocity;
+  // player.x_velocity *= 0.9;// friction
+  // player.y_velocity *= 0.9;// friction
+
+  // if player is falling below floor line
+  if (player.y > 200) {
+    // player.jumping = false;
+    player.y = 200
+    player.y_velocity = 0;
+
+  }
+
+  requestAnimationFrame(loop);
+};
 
 /**************************************************
                 EVENT LISTENERS
 **************************************************/
 window.addEventListener("keydown", event => {
-  if (event.code === "ArrowRight") {
-    moveRight()
-  } else if (event.code === "ArrowLeft") {
-    moveLeft()
+  if (event.code == "ArrowUp" && jumpKey == false) {
+    jumpKey = true
+    player.jumping == true
+    // window.requestAnimationFrame(playerJump)
+    // playerJump()
+    // loop()
+    console.log(jumpKey);
   }
 })
 
 window.addEventListener("keyup", event => {
-  // console.log(event.code);
-  if (event.code === "ArrowRight") {
-    // controls.right = true
-    // console.log(controls.right);
-    stop()
-    console.log("released the right arrow!");
+  if (event.code == "ArrowUp") {
+  jumpKey = false
+  console.log(jumpKey);
   }
 })
+
 
 /**************************************************
                 INVOKING FUNCTIONS
 **************************************************/
-
-// createObstacles()
-// createPizzas()
-// createBackground()
+renderAll()
 // animate()
-firstRender()
 
 
 
